10d7db1e5c24fe246e809eea7a5916cbe1b9e57b
unknown <jesicatapia@gmail.com>
2022-01-13 18:19:15 -0600

    Entrevistas
    Modificación sociedad civil

---
+++ assets/scripts/scanimation.js
@@ -0,0 +1,82 @@
+window.addEventListener('scroll', function() {
+    let animacion = document.getElementById("mambanimado");
+    let posicionimg = animacion.getBoundingClientRect().top;
+    console.log(posicionimg);
+    let tamañoDePantalla = window.innerHeight / 1.5;
+    if (posicionimg < tamañoDePantalla) {
+        animacion.style.animation = 'entrada 1s ease-in-out '
+    }
+
+})
+window.addEventListener('scroll', function() {
+    let animacion = document.getElementById("eduanimado");
+    let posicionimg = animacion.getBoundingClientRect().top;
+    console.log(posicionimg);
+    let tamañoDePantalla = window.innerHeight / 1.5;
+    if (posicionimg < tamañoDePantalla) {
+        animacion.style.animation = 'entrada 1s ease-in-out '
+    }
+
+})
+window.addEventListener('scroll', function() {
+    let animacion = document.getElementById("saludanimado");
+    let posicionimg = animacion.getBoundingClientRect().top;
+    console.log(posicionimg);
+    let tamañoDePantalla = window.innerHeight / 1.5;
+    if (posicionimg < tamañoDePantalla) {
+        animacion.style.animation = 'entrada 1s ease-in-out '
+    }
+
+})
+window.addEventListener('scroll', function() {
+    let animacion = document.getElementById("justiciaanimado");
+    let posicionimg = animacion.getBoundingClientRect().top;
+    console.log(posicionimg);
+    let tamañoDePantalla = window.innerHeight / 1.5;
+    if (posicionimg < tamañoDePantalla) {
+        animacion.style.animation = 'entrada 1s ease-in-out '
+    }
+
+})
+window.addEventListener('scroll', function() {
+    let animacion = document.getElementById("textmambanimado");
+    let posicionimg = animacion.getBoundingClientRect().top;
+    console.log(posicionimg);
+    let tamañoDePantalla = window.innerHeight / 1.5;
+    if (posicionimg < tamañoDePantalla) {
+        animacion.style.animation = 'entradaizquierda 1s ease-in-out '
+    }
+
+})
+
+window.addEventListener('scroll', function() {
+    let animacion = document.getElementById("texteduanimado");
+    let posicionimg = animacion.getBoundingClientRect().top;
+    console.log(posicionimg);
+    let tamañoDePantalla = window.innerHeight / 1.5;
+    if (posicionimg < tamañoDePantalla) {
+        animacion.style.animation = 'entradaderecha 1s ease-in-out '
+    }
+
+})
+window.addEventListener('scroll', function() {
+    let animacion = document.getElementById("textsaludanimado");
+    let posicionimg = animacion.getBoundingClientRect().top;
+    console.log(posicionimg);
+    let tamañoDePantalla = window.innerHeight / 1.5;
+    if (posicionimg < tamañoDePantalla) {
+        animacion.style.animation = 'entradaizquierda 1s ease-in-out '
+    }
+
+})
+
+window.addEventListener('scroll', function() {
+    let animacion = document.getElementById("textjusticianimado");
+    let posicionimg = animacion.getBoundingClientRect().top;
+    console.log(posicionimg);
+    let tamañoDePantalla = window.innerHeight / 1.5;
+    if (posicionimg < tamañoDePantalla) {
+        animacion.style.animation = 'entradaderecha 1s ease-in-out '
+    }
+
+})
